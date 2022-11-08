0d9119a4ddc275f3b3ce10a8ff434681fc044883
Stroove <ttelepov94@mail.ru>
2022-11-08 06:47:23 +0300

    Updates

---
+++ precache-manifest.4cce897dffd414233859d737c08b41a6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d40206a12d6256201d02",
+    "url": "/Stroove/T/static/css/main.04067cb4.chunk.css"
+  },
+  {
+    "revision": "d40206a12d6256201d02",
+    "url": "/Stroove/T/static/js/main.d40206a1.chunk.js"
+  },
+  {
+    "revision": "a3ad4a84603aef25b830",
+    "url": "/Stroove/T/static/js/1.a3ad4a84.chunk.js"
+  },
+  {
+    "revision": "1fb386e352dc36a6509a",
+    "url": "/Stroove/T/static/css/2.62a3ff7d.chunk.css"
+  },
+  {
+    "revision": "1fb386e352dc36a6509a",
+    "url": "/Stroove/T/static/js/2.1fb386e3.chunk.js"
+  },
+  {
+    "revision": "8cec9fa1beb5335d2666",
+    "url": "/Stroove/T/static/js/3.8cec9fa1.chunk.js"
+  },
+  {
+    "revision": "8e4c382077b4961a7f96",
+    "url": "/Stroove/T/static/js/runtime~main.8e4c3820.js"
+  },
+  {
+    "revision": "24b204bdfa2093cb63c296d22cd420c3",
+    "url": "/Stroove/T/static/media/preloaderImg.24b204bd.svg"
+  },
+  {
+    "revision": "2220a5a51ba9cbb881a32af1c90ffcb3",
+    "url": "/Stroove/T/index.html"
+  }
+];
